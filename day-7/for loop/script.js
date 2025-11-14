a0489b03bac2571cc8d7f0e705bd3a0a403367d4
Prince gond <princegondrw123@gmail.com>
2025-11-14 16:59:43 +0530

    Add files via upload

---
+++ day-7/for loop/script.js
@@ -0,0 +1,133 @@
+ //
+
+
+
+//  2.-----> look st thid series 7, 10,8,11,9,12,... what number should come next?
+//  let n = 7;   
+// for(let i=1;i<=8;i++){
+//     document.writeln(n,)
+//      if(i%2==1){
+//         n+=3;
+//      } 
+//      else{
+//          n -= 2;
+//      }
+    
+// }
+
+//3.-----> Look at this series: 36, 34, 30, 28, 24, ... What number should come next?
+
+// let x = 36;
+// for(let i=1; i<=7; i++){
+//     document.writeln(x);
+//     if(i%2==1){
+//         x=x-2;
+
+//     }
+//     else{
+//         x=x-4;
+//     }
+// }
+
+
+// 4.-----> Look at this series: 22, 21, 23, 22, 24, 23, ... What number should come next?
+// let n = 20;
+// for(let i=1;i<=8;i++){
+//     document.writeln(n,);
+//     if(i%2==0){
+//         n=n-1
+//     }
+//     else if(1%2==1){
+//         n=n+2
+//     }   
+// }
+
+
+// 5.Look at this series: 53, 53, 40, 40, 27, 27, ... What number should come next?
+
+// let n = 53;
+// for(let i=1; i<=5;i++){
+     
+//     document.writeln(n);
+//     document.writeln(n);
+//     n=n-13;  
+// } 
+
+ 
+// 6.Look at this series: 21, 9, 21, 11, 21, 13, 21, ... What number should come next?
+// let n=21;
+// let m =9
+// for(let i=1;i<=9;i++){
+//  if(i%2==1){
+//     document.writeln(n)
+//  }
+//  else{
+//     document.writeln(m)
+//     m=m+2;
+//  }
+// }
+
+//// douts no. 6
+
+// 7.Look at this series: 58, 52, 46, 40, 34, ... What number should come next?
+//  let n=58;
+//  for(let i=1;i<=7;i++){
+//     document.writeln(n);
+//     n=n-6;
+//  }
+
+// 8.Look at this series: 3, 4, 7, 8, 11, 12, ... What number should come next?
+// let n=3;
+// for(let i=1;i<=9;i++){
+//     document.writeln(n)
+//     if(i%2==1){
+//         n=n+1;
+//     }
+//     else if(i%2==0){
+//         n=n+3;
+//     }
+// }
+
+
+// 9.Look at this series: 8, 22, 8, 28, 8, ... What number should come next?
+// let n = 8;
+// let m = 22;
+// for(let i=1;i<=8;i++){
+//     if(i%2==1){
+//         document.writeln(n);
+//     }
+//     else{
+//         document.writeln(m);
+//         m=m+4;
+//     }
+// }
+
+
+// 10.Look at this series: 31, 29, 24, 22, 17, ... What number should come next?
+// let n =31;
+// for(let i=1;i<=7;i++){
+//     document.writeln(n)
+//     if(i%2==1){
+//         n=n-2;
+//     }
+//     else 
+//         {
+//         n=n-5  ;
+//     }
+// }
+
+
+// 11.Look at this series: 1.5, 2.3, 3.1, 3.9, ... What number should come next?
+//  let n = 1.5;
+// for(let i=1;i<=7;i++){
+//     document.writeln(n.toFixed (1))
+//     n=n+ 0.8  ;
+// }
+
+
+// 12.Look at this series: 14, 28, 20, 40, 32, 64, ... What number should come next?
+let n=14;
+for(let i=1;i<=8;i++){
+    
+}
+
---
+++ day-7/while loop/script.js
@@ -0,0 +1,71 @@
+//1. ---> 1 to n.
+
+// let i=1 ;
+// let n = 20;
+
+// while(i<=n)
+// {
+//     document.writeln(i,"<br>")
+// i++
+// }
+
+
+ // 
+ //2.----> n to 1
+//  let i = 1;
+//  let n = 20;
+//  while(n>=i){
+//     document.writeln(n,"<br>");
+//     n--;
+//  }
+ 
+
+
+
+
+ //3.--->even number and odd number
+//  let i = 1;
+//  let n=15;
+//  while(i<=n){
+//     if(i%2==0){
+//         document.writeln(i ," ");
+//     }
+//     i++;
+//  }
+
+
+//4.----> odd number from n to 1
+
+// let i =1;
+// let n = 18;
+// while(n>=i){
+//     if(n%2==1){
+//         document.writeln(n,"<br>");
+//     }
+//     n--;
+// }
+
+
+//5.----> leep year start and end 
+
+// let start =2000;
+// let end = 2024;
+// while(start<=end){
+//     if(start%4==0){
+//         document.writeln(start,"<br>");
+//     }
+//     start++;
+// }
+
+
+//6.----> logical fix diffrence 1 11 111  1111 11111
+
+let i =1;
+let num =1;
+while(i<=5){
+    document.writeln(j,);
+    j = j*10 + 1;
+    i++;
+}
+  
+  
