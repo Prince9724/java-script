7e07f86aabd31e1e4e21de3bd3d3352184ced8c2
Prince gond <princegondrw123@gmail.com>
2025-12-03 19:01:48 +0530

    Add files via upload

---
+++ day-8/script.js
@@ -0,0 +1,15 @@
+//table print of number of seven
+// let n = 12;
+// for(let i=1;i<=10;i++){
+//     console.log(n," x"+ i + "=" +n*i );
+// }
+//  function table(){
+
+//  }
+
+function cube(n){
+    return n*n*n;
+}
+let num = 6;
+result = cube(num);
+document.writeln("cube is " + result);
