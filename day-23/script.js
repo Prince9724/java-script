1ad4c5ff7bccb1e068dd6c2ee93d3120c834f096
Prince gond <princegondrw123@gmail.com>
2025-12-26 15:53:58 +0530

    Add files via upload

--- day-23/script.js
+++ day-23/script.js
@@ -16,14 +16,9 @@ const title_input = document.getElementById("title");//section 2 ke ander first 
 //title_input ke ander store krva rhe hai.
 
 const description_input = document.getElementById("description");
-
-    let notes = [
-    {
-        title: " afg ",
-        description: " fsw ",
-    },
-
-];
+    let isUpdate = false;//to perform update and edit on same button 
+    let updateIndex = 0;
+    let notes = [];
 const addNote = () => {
 
     const title = title_input.value;
@@ -32,7 +27,7 @@ const addNote = () => {
         title: title,
         description: description,
     };
-    notes.push(note);
+    notes.push(note);//new value come in note.
     //local storage accept only string 
     localStorage.setItem("notes-array", JSON.stringify(notes));//notes-array is key name in local storage.
     //  and JSON.stringify is a fuction its will be use to convirt array into string. !!
@@ -44,19 +39,58 @@ const addNote = () => {
 const displayNotes = () => {
     note_container.innerHTML = "";
     const data = localStorage.getItem("notes-array");//notes-array ko display pr get krne ke liye data name ka variable 
-//bna ke usko usme store krvaya.
-  let notes =  JSON.parse(data);//JSON.parse is a function it will be use to convirt string into array .
-    notes.forEach((note) => {
+//bna ke usko usme store krvaya.but data is sttring.
+   if(data){//data ki value null  hogi to.
+        notes =  JSON.parse(data);
+    }
+   // data comes from local storage . and store into actual array.
+  //  JSON.parse is a function it will be use to convirt string into array .
+    notes.forEach((note,i) => {
         const div = document.createElement("div");
         div.className = "card w-25 m-2";
         div.innerHTML = `<div class="card-body">
         <h5 class="card-title">${note.title} </h5>
         <p class="card-text">${note.description}</p>
-        <a href="#" class="btn btn-warning">Edit</a>
-        <a href="#" class="btn btn-danger">Delete</a>
+        <button  onClick="updateNotes(${i})" class="btn btn-warning">Edit</button>
+        <a href="#"onclick="deleteNote(${i})" class="btn btn-danger">Delete</a> 
     </div>`;
         note_container.appendChild(div);
     })
 }
+
+const deleteNote=(i)=>{
+    notes.splice(i,1); //splice  name ki property delete krne ke liye use hoti hai. !!
+    localStorage.setItem("notes-array", JSON.stringify(notes));//becuse delete ke  baad ka jo data hai vo fir local storage me store ho jayega.
+//aur displarNotes ko call krya hai taki update  data display ho jaye 
+    displayNotes();
+}
+const updateNotes=(i)=>{
+    isUpdate=true;
+    updateIndex = i;
+    title_input.value = notes[i].title;
+    description_input.value=notes[i].description;
+    add_note_button.textContent="update notes";
+}
+const editNote=()=>{
+    notes[updateIndex].title=title_input.value;
+    notes[updateIndex].description = description_input.value;
+    localStorage.setItem("notes-array",JSON.stringify(notes));
+    isUpdate = false; // wapas add mode
+    title_input.value = "";
+    description_input.value = "";
+    displayNotes();
+    add_note_button.textContent="Add note";
+}
+
 displayNotes();
-add_note_button.addEventListener("click", addNote);
+
+const handleUpdateAndEdit =()=>{
+    if(isUpdate){
+        editNote();
+    }else{
+addNote();
+    }
+
+}
+add_note_button.addEventListener("click",  handleUpdateAndEdit);
+//29.45 sec
