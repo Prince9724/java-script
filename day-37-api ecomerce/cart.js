3cb47496fd5b39bd68d7cf665081a24fd3aba53d
Prince gond <princegondrw123@gmail.com>
2026-01-05 16:18:59 +0530

    Add files via upload

--- day-37-api ecomerce/cart.js
+++ day-37-api ecomerce/cart.js
@@ -1,3 +1,8 @@
 
-const container =document.getElementById("container");
- 
+const container = document.getElementById("container");
+  localStorage.getItem("key",JSON.stringify())
+
+
+ const fetchProduct=()=>{
+  
+  }
--- day-37-api ecomerce/script.js
+++ day-37-api ecomerce/script.js
@@ -1,52 +1,76 @@
 // {
-    // <div class="card" style="width: 18rem;">
-    //                 <img src="..." class="card-img-top" alt="...">
-    //                 <div class="card-body">
-    //                     <h5 class="card-title">Card title</h5>
-    //                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of
-    //                         the card’s content.</p>
-    //                 </div>
-    //                 <ul class="list-group list-group-flush">
-    //                     <li class="list-group-item">An item</li>
-    //                     <li class="list-group-item">A second item</li>
-    //                     <li class="list-group-item">A third item</li>
-    //                 </ul>
-    //                 <div class="card-body">
-    //                     <button class="btn btn-primary">view more </button>
-    //                     <button class="btn btn-success"> Add to chart </button>
-    //                 </div>
-    //             </div>
+// <div class="card" style="width: 18rem;">
+//                 <img src="..." class="card-img-top" alt="...">
+//                 <div class="card-body">
+//                     <h5 class="card-title">Card title</h5>
+//                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+//                         the card’s content.</p>
+//                 </div>
+//                 <ul class="list-group list-group-flush">
+//                     <li class="list-group-item">An item</li>
+//                     <li class="list-group-item">A second item</li>
+//                     <li class="list-group-item">A third item</li>
+//                 </ul>
+//                 <div class="card-body">
+//                     <button class="btn btn-primary">view more </button>
+//                     <button class="btn btn-success"> Add to chart </button>
+//                 </div>
+//             </div>
 // }
 
+
+let cart = []; //ek hmne global array bnya jo khali h 
+const addToCart = (product) => {
+    let cart = localStorage.getItem("cart");
+    if (cart === null) {
+        cart = [];
+    } else {
+        cart = JSON.parse(cart);
+    }
+    // console.log(cart);
+    cart.push({
+        productimg: product.thumbnail,
+        id: product.id,
+        title: product.title,
+        price: product.price,
+    });
+    localStorage.setItem("cart", JSON.stringify(cart));
+}
+
 const container = document.getElementById("container");
-    const displayProduct=(products)=>{
-        products.forEach((product)=>{
-            const div = document.createElement("div");
-            div.innerHTML=`<div class="card" style="width: 18rem;">
-                    <img src="${product.images}" class="card-img-top" alt="...">
+const displayProduct = (products) => {
+    products.forEach((product) => {
+        const div = document.createElement("div");
+        div.innerHTML = `<div class="card" style="width: 18rem;">
+                    <img src="${product.thumbnail}" class="card-img-top" alt="...">
                     <div class="card-body">
                         <h5 class="card-title"> ${product.title}</h5>
                         <p class="card-text"> ${product.description}</p>
                     </div>
                     <ul class="list-group list-group-flush">
-                        <li class="list-group-item"> ${product.price}</li>
+                        <li class="list-group-item"> Price  ${product.price}</li>
                         <li class="list-group-item"></li>
                         <li class="list-group-item"><span>Discount  -${product.discountPercentage}%</span><span> | ${product.rating} ⭐ <span>rating</span></span></li>
                         <li class="list-group-item"> </li>
                     </ul>
                     <div class="card-body">
                         <button class="btn btn-primary">view more </button>
-                        <button class="btn btn-success"> Add to chart </button>
+                        <button onclick="${addToCart(product)}" class="btn btn-success"> Add to chart </button>
                     </div>
                 </div>`;
-                container.appendChild(div);
-        });
-    }
 
-const fetchProduct = ()=>{
-    fetch("https://dummyjson.com/product")
-    .then((res)=>res.json())
-    .then((data)=>displayProduct(data.products))
+        container.appendChild(div);
+    });
+}
+
+const fetchProduct = () => {
+    fetch("https://dummyjson.com/products")
+        .then((res) => res.json())
+        .then((data) => displayProduct(data.products))
+
 }
 fetchProduct();
+
+
+
 // priyanshu
