2752a976096c876a10f695c1c771ec839ead1e1c
Prince gond <princegondrw123@gmail.com>
2026-02-12 17:09:03 +0530

    Add files via upload

---
+++ JAVASCRIPT- Revision/day-1/script.js
@@ -0,0 +1,7 @@
+const group = document.querySelector(".group").children;
+// group.textContent += "#"; 
+ for(let i =0; i<group.length; i++){
+    group[i].textContent = group[i].textContent+"#";
+    //text content me ham # ko jodne ke liye rkha hai 
+ }
+ 
